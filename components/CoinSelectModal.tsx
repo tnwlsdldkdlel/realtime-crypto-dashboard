3bb5f3854fd1419cb2b3b1e9c66b5b28bad0aceb
tnwlsdldkdlel <tnwls2dkdlel@naver.com>
2025-12-02 11:34:21 +0900

    fix: CoinSelectModal ESLint 경고 수정
    - useEffect 내 setState 호출을 useRef로 이전 상태 추적하여 개선
    - 모달이 닫혔다가 열릴 때만 상태 초기화

--- components/CoinSelectModal.tsx
+++ components/CoinSelectModal.tsx
@@ -36,8 +36,12 @@ export default function CoinSelectModal({
   useEffect(() => {
     // 모달이 닫혔다가 열릴 때만 초기화
     if (isOpen && !prevIsOpenRef.current) {
+      // 모달 열릴 때 상태 초기화는 의도된 동작이므로 허용
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setSearchQuery('');
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setHighlightedIndex(0);
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setDisplayCount(50);
       
       if (searchInputRef.current) {
